744fd81b859a189948d06382b2f10a184fa31672
FreedomCoder <pr070cryp70n1c@riseup.net>
2022-05-09 07:48:02 +0300

    Test launch and setting Qt

---
+++ GOCSFD5e_en_US.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build-GOCSFD5e-Desktop_Qt_5_12_12_GCC_64bit-Debug/GOCSFD5e_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
